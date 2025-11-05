cc86488f1077d54f5b9b55377b3948e078d5690a
Max Charlamb <maxcharlamb@microsoft.com>
2025-11-05 10:40:08 -0500

    fix saving datadescriptors in minidumps

--- src/coreclr/debug/daccess/enummem.cpp
+++ src/coreclr/debug/daccess/enummem.cpp
@@ -1670,7 +1670,7 @@ HRESULT ClrDataAccess::EnumMemDataDescriptors(CLRDataEnumMemoryFlags flags)
             (TADDR)pContractDescriptor->pointer_data + subDescriptorIndex * sizeof(void*),
             (PBYTE)&pSubDescriptorAddr, sizeof(TADDR), &bytesRead))
             || bytesRead != sizeof(TADDR)
-            || pSubDescriptorAddr == NULL)
+            || pSubDescriptorAddr == 0)
         {
             continue;
         }
@@ -1680,7 +1680,7 @@ HRESULT ClrDataAccess::EnumMemDataDescriptors(CLRDataEnumMemoryFlags flags)
             pSubDescriptorAddr,
             (PBYTE)&subDescriptorAddr, sizeof(TADDR), &bytesRead))
             || bytesRead != sizeof(TADDR)
-            || subDescriptorAddr == NULL)
+            || subDescriptorAddr == 0)
         {
             continue;
         }
