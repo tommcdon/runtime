259bc910b1e25d1949f43092423a13cf0555d95a
rcj1 <rachel.jarvi@gmail.com>
2025-11-06 15:28:55 -0800

    continuation object reading with cordb

--- src/coreclr/debug/daccess/dacdbiimpl.cpp
+++ src/coreclr/debug/daccess/dacdbiimpl.cpp
@@ -2339,6 +2339,10 @@ void DacDbiInterfaceImpl::GetClassTypeInfo(TypeHandle                      typeH
                                            DebuggerIPCE_ExpandedTypeData * pTypeInfo,
                                            AppDomain *                     pAppDomain)
 {
+    if (typeHandle.AsMethodTable()->IsContinuation())
+    {
+        typeHandle = TypeHandle(g_pContinuationClassIfSubTypeCreated);
+    }
     Module * pModule = typeHandle.GetModule();
 
     if (typeHandle.HasInstantiation()) // the type handle represents a generic instantiation
@@ -2426,9 +2430,13 @@ void DacDbiInterfaceImpl::TypeHandleToBasicTypeInfo(TypeHandle                  
         case ELEMENT_TYPE_CLASS:
         case ELEMENT_TYPE_VALUETYPE:
         {
-            Module * pModule = typeHandle.GetModule();
+            if (typeHandle.AsMethodTable()->IsContinuation())
+            {
+                typeHandle = TypeHandle(g_pContinuationClassIfSubTypeCreated);
+            }
 
-            if (typeHandle.HasInstantiation())   // only set if instantiated
+            Module * pModule = typeHandle.GetModule();
+            if (typeHandle.HasInstantiation()) // the type handle represents a generic instantiation
             {
                 pTypeInfo->vmTypeHandle.SetDacTargetPtr(typeHandle.AsTAddr());
             }
--- src/coreclr/debug/ee/debugger.cpp
+++ src/coreclr/debug/ee/debugger.cpp
@@ -11664,6 +11664,10 @@ void Debugger::TypeHandleToBasicTypeInfo(AppDomain *pAppDomain, TypeHandle th, D
     case ELEMENT_TYPE_CLASS:
     case ELEMENT_TYPE_VALUETYPE:
         {
+            if (th.AsMethodTable()->IsContinuation())
+            {
+                th = TypeHandle(g_pContinuationClassIfSubTypeCreated);
+            }
             res->vmTypeHandle = th.HasInstantiation() ? WrapTypeHandle(th) : VMPTR_TypeHandle::NullPtr();
                                                                              // only set if instantiated
             res->metadataToken = th.GetCl();
@@ -11746,6 +11750,10 @@ void Debugger::TypeHandleToExpandedTypeInfo(AreValueTypesBoxed boxed,
     case ELEMENT_TYPE_CLASS:
         {
 treatAllValuesAsBoxed:
+            if (th.AsMethodTable()->IsContinuation())
+            {
+                th = TypeHandle(g_pContinuationClassIfSubTypeCreated);
+            }
             res->ClassTypeData.typeHandle = th.HasInstantiation() ? WrapTypeHandle(th) : VMPTR_TypeHandle::NullPtr(); // only set if instantiated
             res->ClassTypeData.metadataToken = th.GetCl();
             DebuggerModule * pModule = LookupOrCreateModule(th.GetModule());
