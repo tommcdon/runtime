2426451f5ac58a5bfbcaf9eeba31fad882ed2a58
rcj1 <rachel.jarvi@gmail.com>
2025-11-07 11:30:17 -0800

    code review

--- src/coreclr/debug/ee/debugger.cpp
+++ src/coreclr/debug/ee/debugger.cpp
@@ -11664,10 +11664,7 @@ void Debugger::TypeHandleToBasicTypeInfo(AppDomain *pAppDomain, TypeHandle th, D
     case ELEMENT_TYPE_CLASS:
     case ELEMENT_TYPE_VALUETYPE:
         {
-            if (th.AsMethodTable()->IsContinuation())
-            {
-                th = TypeHandle(g_pContinuationClassIfSubTypeCreated);
-            }
+            th = th.UpCastTypeIfNeeded();
             res->vmTypeHandle = th.HasInstantiation() ? WrapTypeHandle(th) : VMPTR_TypeHandle::NullPtr();
                                                                              // only set if instantiated
             res->metadataToken = th.GetCl();
@@ -11750,10 +11747,7 @@ void Debugger::TypeHandleToExpandedTypeInfo(AreValueTypesBoxed boxed,
     case ELEMENT_TYPE_CLASS:
         {
 treatAllValuesAsBoxed:
-            if (th.AsMethodTable()->IsContinuation())
-            {
-                th = TypeHandle(g_pContinuationClassIfSubTypeCreated);
-            }
+            th = th.UpCastTypeIfNeeded();
             res->ClassTypeData.typeHandle = th.HasInstantiation() ? WrapTypeHandle(th) : VMPTR_TypeHandle::NullPtr(); // only set if instantiated
             res->ClassTypeData.metadataToken = th.GetCl();
             DebuggerModule * pModule = LookupOrCreateModule(th.GetModule());
