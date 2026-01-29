7395e9c0b146f5eed3dfb88921053eae7fc66f28
rcj1 <rachel.jarvi@gmail.com>
2026-01-29 10:37:25 -0800

    TBD: remove unneeded updates

--- src/coreclr/System.Private.CoreLib/src/System/Runtime/CompilerServices/AsyncHelpers.CoreCLR.cs
+++ src/coreclr/System.Private.CoreLib/src/System/Runtime/CompilerServices/AsyncHelpers.CoreCLR.cs
@@ -506,7 +506,7 @@ namespace System.Runtime.CompilerServices
 
                         if (newContinuation != null)
                         {
-                            Task.SetRuntimeAsyncContinuationTicks(newContinuation, tickCount);
+                            Task.UpdateRuntimeAsyncContinuationTicks(newContinuation, tickCount);
                             newContinuation.Next = nextContinuation;
                             HandleSuspended();
                             contexts.Pop();
