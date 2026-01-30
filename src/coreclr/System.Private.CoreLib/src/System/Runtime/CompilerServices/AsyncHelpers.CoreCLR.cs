377e8547af280b981d028db293a1b47cb6265e59
rcj1 <rachel.jarvi@gmail.com>
2026-01-30 10:51:23 -0800

    adding IDs etc

--- src/coreclr/System.Private.CoreLib/src/System/Runtime/CompilerServices/AsyncHelpers.CoreCLR.cs
+++ src/coreclr/System.Private.CoreLib/src/System/Runtime/CompilerServices/AsyncHelpers.CoreCLR.cs
@@ -268,7 +268,6 @@ namespace System.Runtime.CompilerServices
             Continuation newContinuation = (Continuation)RuntimeTypeHandle.InternalAllocNoChecks(contMT);
 #endif
             prevContinuation.Next = newContinuation;
-            Task.SetRuntimeAsyncContinuationTicks(newContinuation, Environment.TickCount64);
             return newContinuation;
         }
 
@@ -279,7 +278,6 @@ namespace System.Runtime.CompilerServices
             Continuation newContinuation = (Continuation)RuntimeTypeHandle.InternalAllocNoChecks(contMT);
             Unsafe.As<byte, object?>(ref Unsafe.Add(ref RuntimeHelpers.GetRawData(newContinuation), keepAliveOffset)) = loaderAllocator;
             prevContinuation.Next = newContinuation;
-            Task.SetRuntimeAsyncContinuationTicks(newContinuation, Environment.TickCount64);
             return newContinuation;
         }
 
@@ -293,7 +291,6 @@ namespace System.Runtime.CompilerServices
             {
                 Unsafe.As<byte, object?>(ref Unsafe.Add(ref RuntimeHelpers.GetRawData(newContinuation), keepAliveOffset)) = GCHandle.FromIntPtr(loaderAllocatorHandle).Target;
             }
-            Task.SetRuntimeAsyncContinuationTicks(newContinuation, Environment.TickCount64);
             return newContinuation;
         }
 #endif
@@ -370,7 +367,7 @@ namespace System.Runtime.CompilerServices
             {
                 Debug.Assert(m_stateObject == null);
                 m_stateObject = value;
-                Task.SetRuntimeAsyncContinuationTicks(value, Environment.TickCount64);
+                Task.SetRuntimeAsyncContinuationTicks(value, Stopwatch.GetTimestamp());
             }
 
             internal void HandleSuspended()
@@ -405,6 +402,13 @@ namespace System.Runtime.CompilerServices
                 Debug.Assert((headContinuation.Flags & continueFlags) == 0);
 
                 SetContinuationState(headContinuation);
+
+                Continuation? nc = headContinuation.Next;
+                while (nc != null)
+                {
+                    Task.SetRuntimeAsyncContinuationTicks(nc, Stopwatch.GetTimestamp());
+                    nc = nc.Next;
+                }
 
                 try
                 {
@@ -498,15 +502,15 @@ namespace System.Runtime.CompilerServices
                         asyncDispatcherInfo.NextContinuation = nextContinuation;
 
                         ref byte resultLoc = ref nextContinuation != null ? ref nextContinuation.GetResultStorageOrNull() : ref GetResultStorage();
-                        long tickCount = Task.GetRuntimeAsyncContinuationTicks(curContinuation, out long tickCountVal) ? tickCountVal : Environment.TickCount64;
-                        Task.UpdateRuntimeAsyncTaskTicks(this, tickCount);
+                        RuntimeAsyncContinuationDebugInfo debugInfo = Task.GetRuntimeAsyncContinuationDebugInfo(curContinuation, out RuntimeAsyncContinuationDebugInfo debugInfoVal) ? debugInfoVal : new RuntimeAsyncContinuationDebugInfo(Stopwatch.GetTimestamp());
+                        Task.UpdateRuntimeAsyncTaskTicks(this, debugInfo.TickCount);
                         Continuation? newContinuation = curContinuation.ResumeInfo->Resume(curContinuation, ref resultLoc);
 
                         Task.RemoveRuntimeAsyncContinuationTicks(curContinuation);
 
                         if (newContinuation != null)
                         {
-                            Task.UpdateRuntimeAsyncContinuationTicks(newContinuation, tickCount);
+                            Task.UpdateRuntimeAsyncContinuationDebugInfo(newContinuation, debugInfo);
                             newContinuation.Next = nextContinuation;
                             HandleSuspended();
                             contexts.Pop();
@@ -593,6 +597,7 @@ namespace System.Runtime.CompilerServices
                     if (continuation == null || (continuation.Flags & ContinuationFlags.HasException) != 0)
                         return continuation;
 
+                    RemoveRuntimeAsyncContinuationTicks(continuation);
                     continuation = continuation.Next;
                 }
             }
