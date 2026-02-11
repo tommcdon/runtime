351026889d3a33c906897a60c365dce1543bf118
Tom McDonald <tommcdon@microsoft.com>
2026-02-10 23:34:16 -0500

    Remove unused static constructor from RuntimeAsyncTask

--- src/coreclr/System.Private.CoreLib/src/System/Runtime/CompilerServices/AsyncHelpers.CoreCLR.cs
+++ src/coreclr/System.Private.CoreLib/src/System/Runtime/CompilerServices/AsyncHelpers.CoreCLR.cs
@@ -347,11 +347,6 @@ namespace System.Runtime.CompilerServices
                 m_stateFlags |= (int)InternalTaskOptions.HiddenState;
             }
 
-            [MethodImpl(MethodImplOptions.NoOptimization | MethodImplOptions.NoInlining)]
-            static RuntimeAsyncTask()
-            {
-            }
-
             internal override void ExecuteFromThreadPool(Thread threadPoolThread)
             {
                 DispatchContinuations();
