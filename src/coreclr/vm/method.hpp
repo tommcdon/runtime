68deac5772a8daf785701f9822fe2935829af0c6
Max Charlamb <44248479+max-charlamb@users.noreply.github.com>
2026-02-17 16:54:57 -0500

    dacize and use FindOrCreateAssociatedMethodDesc

--- src/coreclr/vm/method.hpp
+++ src/coreclr/vm/method.hpp
@@ -1721,6 +1721,11 @@ public:
         return FindOrCreateAssociatedMethodDesc(this, GetMethodTable(), FALSE, GetMethodInstantiation(), allowInstParam, FALSE, TRUE, AsyncVariantLookup::AsyncOtherVariant);
     }
 
+    MethodDesc* GetAsyncOtherVariantNoCreate(BOOL allowInstParam = TRUE)
+    {
+        return FindOrCreateAssociatedMethodDesc(this, GetMethodTable(), FALSE, GetMethodInstantiation(), allowInstParam, FALSE, FALSE, AsyncVariantLookup::AsyncOtherVariant);
+    }
+
     MethodDesc* GetAsyncVariant(BOOL allowInstParam = TRUE)
     {
         _ASSERT(!IsAsyncVariantMethod());
