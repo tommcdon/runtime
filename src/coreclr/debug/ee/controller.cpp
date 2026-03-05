1251a8acadc4d80d38471778250ced5d2383f6e4
Tom McDonald <tommcdon@microsoft.com>
2026-03-05 09:47:10 -0500

    Disable runtime assert for empty stacks

--- src/coreclr/debug/ee/controller.cpp
+++ src/coreclr/debug/ee/controller.cpp
@@ -496,7 +496,7 @@ void ControllerStackInfo::GetStackInfo(
     if (result == SWA_DONE)
     {
         _ASSERTE(!HasReturnFrame()); // We didn't find a managed return frame
-        _ASSERTE(HasReturnFrame(true)); // All threads have at least one unmanaged frame
+        //_ASSERTE(HasReturnFrame(true)); // All threads have at least one unmanaged frame
     }
 }
 
